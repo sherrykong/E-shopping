291998a41f4a62935812f2b88d8f54f8123f78ba
sherrykong <2806651841@qq.com>
2017-05-24 21:48:35 +0800

    fix

---
+++ HelloSprintMVC/src/main/java/com/yibai/springmvc/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.yibai.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	@RequestMapping("/hello")
+	public String hello(Model model) {
+		
+		model.addAttribute("greeting", "Hello Spring MVC");
+		return "helloworld";
+	}
+}
